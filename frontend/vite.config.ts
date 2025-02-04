979901114bad21dcedf0dc47ee75cb3a6359b84d
PawelMisztal <mrpawolo@gmail.com>
2025-02-04 22:20:08 +0100

    https vite

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,16 +1,20 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+import mkcert from "vite-plugin-mkcert";
 
 // https://vite.dev/config/
 export default defineConfig({
-    plugins: [react()],
+    plugins: [react(), mkcert()],
     server: {
+        // https: true,
         port: 3001,
         host: true,
         proxy: {
             "/api": {
                 target: "http://localhost:3000/api",
                 changeOrigin: true,
+                secure: false,
+                ssl: false,
                 rewrite: (path) => path.replace(/^\/api/, ""),
             },
         },
