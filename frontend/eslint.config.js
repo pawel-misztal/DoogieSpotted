702670d3cfadd91701092160ede91f65f13020d9
PawelMisztal <mrpawolo@gmail.com>
2025-02-06 09:54:41 +0100

    add react compiler

--- frontend/eslint.config.js
+++ frontend/eslint.config.js
@@ -3,6 +3,7 @@ import globals from "globals";
 import reactHooks from "eslint-plugin-react-hooks";
 import reactRefresh from "eslint-plugin-react-refresh";
 import tseslint from "typescript-eslint";
+import reactCompiler from "eslint-plugin-react-compiler";
 
 export default tseslint.config(
     { ignores: ["dist"] },
@@ -16,6 +17,7 @@ export default tseslint.config(
         plugins: {
             "react-hooks": reactHooks,
             "react-refresh": reactRefresh,
+            // "react-compiler": reactCompiler,
         },
         rules: {
             ...reactHooks.configs.recommended.rules,
@@ -25,6 +27,7 @@ export default tseslint.config(
             ],
             "@typescript-eslint/no-unused-vars": "warn",
             "@typescript-eslint/no-explicit-any": "warn",
+            // "react-compiler/react-compiler": "warn",
         },
     }
 );
--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -2,9 +2,20 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import mkcert from "vite-plugin-mkcert";
 
+const ReactCompilerConfig = {
+    target: "18", // '17' | '18' | '19'
+};
+
 // https://vite.dev/config/
 export default defineConfig({
-    plugins: [react(), mkcert()],
+    plugins: [
+        react({
+            babel: {
+                plugins: [["babel-plugin-react-compiler", ReactCompilerConfig]],
+            },
+        }),
+        mkcert(),
+    ],
     server: {
         // https: true,
         port: 3001,
